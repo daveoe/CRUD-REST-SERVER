b6ea1289f0a89d276f19278ec47856dbdc351932
Daveolivares <daveoworking@gmail.com>
2019-03-10 23:53:25 -0300

    Variable ENV MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -22,7 +22,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = "mongodb://localhost:27017/cafe";
 } else {
-    urlDB = "mongodb+srv://Daveolivares:X44f6MKAYFDNBhDR@cluster0-m0vf7.mongodb.net/cafe";
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
