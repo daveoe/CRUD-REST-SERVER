2b6817d84f8221670d85f11a2f0b091bc1d7566f
Daveolivares <daveoworking@gmail.com>
2019-03-10 23:12:21 -0300

    Checking URL

--- server/config/config.js
+++ server/config/config.js
@@ -19,10 +19,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 // =========================
 let urlDB;
 
-if (process.env.NODE_ENV === 'dev') {
-    urlDB = "mongodb://localhost:27017/cafe";
-} else {
+// if (process.env.NODE_ENV === 'dev') {
+//     urlDB = "mongodb://localhost:27017/cafe";
+// } else {
     urlDB = "mongodb+srv://Daveolivares:X44f6MKAYFDNBhDR@cluster0-m0vf7.mongodb.net/cafe";
-}
+// }
 
 process.env.URLDB = urlDB;
